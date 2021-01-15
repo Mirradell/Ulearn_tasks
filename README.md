41115ec6e75a4e6d79615d19af5057a7ab08252b
Mirradell <olya.shtejn@yandex.ru>
2021-01-15 22:02:41 +0300

    Add part 1 module 2

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Ulearn_tasks
+
+## Most of tasks from ulearn.me C# courses(parts 1, 2).
---
+++ part 1/2. Ошибки/Billiards/BilliardsTask.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Billiards
+{
+	public static class BilliardsTask
+	{
+		public static double BounceWall(double directionRadians, double wallInclinationRadians)
+		{
+            return 2 * wallInclinationRadians - directionRadians;
+		}
+	}
+}
